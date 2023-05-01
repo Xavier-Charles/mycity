c23e9257e16b463a479cf7e05217d0f54252ea3f
Xharles <charlesnzubechukwu000@gmail.com>
2023-05-01 09:04:25 +0100

    add street lights status

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -27,12 +27,20 @@ const Status = ({ selectedCityId }) => {
     ),
   };
 
-  const powerStatus = 0
+  const powerStatus = 0;
+  const lightsStatus = 0;
 
   const powerDescription = {
     0: "Power is currently out",
     1: "Power is currently on",
   };
+
+  const lightsDescription = {
+    0: "Lights are out",
+    1: "Lights are on",
+  };
+
+
   return (
     <div className="bg-gray-900 py-6">
       <div className="pointer-events-none  sm:px-6 sm:pb-5 lg:px-8">
@@ -69,6 +77,25 @@ const Status = ({ selectedCityId }) => {
                 &rarr;
               </span>
               {powerDescription[powerStatus]}
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className="pointer-events-none  sm:px-6 sm:pb-5 lg:px-8 pt-4">
+        <div className="pointer-events-auto flex items-center justify-between gap-x-6 bg-gray-800 max-w-7xl mx-auto px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5">
+          <p className="text-lg leading-6 text-secondary">
+            <a href="#">
+              <strong className="font-semibold">
+                Street Light Status{" "}
+                <span className="font-normal italic">
+                  ({moment().format("DD/MM/YY : HH:mm")})
+                </span>
+              </strong>
+
+              <span className="px-3" aria-hidden="true">
+                &rarr;
+              </span>
+              {lightsDescription[lightsStatus]}
             </a>
           </p>
         </div>
