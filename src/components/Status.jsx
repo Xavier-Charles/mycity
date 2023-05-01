046ddf1a7237a972a8560e3bb284c94f9cc71b69
Xharles <charlesnzubechukwu000@gmail.com>
2023-05-01 22:40:00 +0100

    add padding to bottom

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -65,7 +65,7 @@ const Status = ({ selectedCityId }) => {
   }, []);
 
   return (
-    <div className="bg-gray-900 py-6">
+    <div className="bg-gray-900 py-6 pb-16">
       <div className="pointer-events-none  sm:px-6 sm:pb-5 lg:px-8">
         <div className="pointer-events-auto flex items-center justify-between gap-x-6 bg-gray-800 max-w-7xl mx-auto px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5">
           <p className="text-lg leading-6 text-secondary">
